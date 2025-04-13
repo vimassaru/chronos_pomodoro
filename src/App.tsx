fe0abc63a306d0ab648217d187a0bf5ae7d83945
Vinicius Tanaka <vi_massaru.tanaka@hotmail.com>
2025-04-13 16:12:29 -0300

    feature: configurando CSS e Heading.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { TimerIcon } from 'lucide-react';
+import { Heading } from './components/ui/Heading';
 
-function App() {
-  const [count, setCount] = useState(0)
+import './styles/global.css';
+import './styles/themes.css';
 
+export const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <Heading>
+        Hello World
+        <button>
+          <TimerIcon />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </Heading>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
     </>
-  )
-}
-
-export default App
+  );
+};
