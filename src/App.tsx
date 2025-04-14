266747a6c1b70b91b4a099a948bc478fcbef93eb
Vinicius Tanaka <vi_massaru.tanaka@hotmail.com>
2025-04-14 00:59:26 -0300

    estruturando componentes UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/ui/Container';
 import { Heading } from './components/ui/Heading';
-
 import './styles/global.css';
 import './styles/themes.css';
 
 export const App = () => {
   return (
     <>
-      <Heading>
-        Hello World
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>Lorem ipsum dolor sit amet.</p>
-      <p>Lorem ipsum dolor sit amet.</p>
-      <p>Lorem ipsum dolor sit amet.</p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 };
---
+++ src/components/ui/Container.tsx
@@ -0,0 +1,13 @@
+import styles from './Container.module.css';
+
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+export const Container = ({ children }: ContainerProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
+};
