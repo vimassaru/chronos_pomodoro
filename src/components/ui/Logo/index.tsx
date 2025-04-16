1b250947b0a25e0133810a80987338509d61e182
Vinicius Tanaka <vinicius.tanaka@proaero.aero>
2025-04-16 13:52:08 -0300

    criando componente Logo

--- src/components/ui/Logo/index.tsx
+++ src/components/ui/Logo/index.tsx
@@ -2,10 +2,12 @@ import { TimerIcon } from 'lucide-react';
 import styles from './styles.module.css';
 
 export const Logo = () => {
-  return <div className={styles.logo}>
-    <a href="#">
-      <TimerIcon />
-      <span>Chronos</span>
-    </a>
-  </div>;
+  return (
+    <div className={styles.logo}>
+      <a href="#">
+        <TimerIcon />
+        <span>Chronos</span>
+      </a>
+    </div>
+  )
 };
