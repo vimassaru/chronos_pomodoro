1129d9444a99fdb089bfd9a74c64c06aedc1cb77
Vinicius Tanaka <vinicius.tanaka@proaero.aero>
2025-04-25 13:59:26 -0300

    feature: adicionando DefaultButton.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { PlayCircleIcon } from 'lucide-react';
 import { Container } from './components/ui/Container';
 import { CountDown } from './components/ui/CountDown';
 import { Cycles } from './components/ui/Cycles';
+import { DefaultButton } from './components/ui/DefaultButton';
 import { DefaultInput } from './components/ui/DefaultInput';
 import { Logo } from './components/ui/Logo';
 import { Menu } from './components/ui/Menu';
@@ -40,7 +42,7 @@ export const App = () => {
           </div>
 
           <div className='formRow'>
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
       </Container>
